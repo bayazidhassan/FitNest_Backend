451ab52a8ac3a72646c00c70c838ec6f3569dd49
bayazidhassan <bayazidhassan776@gmail.com>
2025-12-01 03:15:21 +0600

    update CORS configuration

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
+import cors from 'cors';
 import express from 'express';
 import router from './app/routes';
-import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(
+  cors({ origin: ['https://fitnestbd.netlify.app', 'http://localhost:5173'], credentials: true }),
+);
 
 app.use('/api/v1', router);
 
