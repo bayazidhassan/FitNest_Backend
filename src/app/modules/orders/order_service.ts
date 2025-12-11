8f750d15a4437ca2099387152525da5e791af6cb
bayazidhassan <bayazidhassan776@gmail.com>
2025-12-12 00:06:24 +0600

    create order into DB

--- src/app/modules/orders/order_service.ts
+++ src/app/modules/orders/order_service.ts
@@ -1,7 +1,13 @@
 import { TOrder } from './order_interface';
+import { Order } from './order_model';
 
 const placeOrderIntoDB = async (payload: TOrder) => {
-  console.log(payload);
+  const result = await Order.create(payload);
+
+  if (!result) {
+    throw new Error('Order is not placed');
+  }
+  return result;
 };
 
 export const orderService = {
