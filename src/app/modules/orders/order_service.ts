741eedf98b1029f792f167dfa9a0112a4d15c14f
bayazidhassan <bayazidhassan776@gmail.com>
2025-12-24 05:47:49 +0600

    improve some logic in updateStatus

--- src/app/modules/orders/order_service.ts
+++ src/app/modules/orders/order_service.ts
@@ -26,8 +26,6 @@ const getOrdersByStatusFromDB = async (status: string) => {
   }
 };
 
-//const updateOrderStatusIntoDB = async (id: string, status: TStatus) => {
-//for -> when an admin clicks a button, you only update the order if it is still in the expected status.
 const updateOrderStatusIntoDB = async (id: string, fromStatus: TStatus, toStatus: TStatus) => {
   const session = await mongoose.startSession();
 
@@ -39,19 +37,6 @@ const updateOrderStatusIntoDB = async (id: string, fromStatus: TStatus, toStatus
     if (!order) {
       throw new Error('Order not found.');
     }
-    const allowedTransitions: Record<TStatus, TStatus[]> = {
-      pending: ['confirmed', 'cancelled'],
-      confirmed: ['processing', 'cancelled'],
-      processing: ['shipped', 'cancelled'],
-      shipped: ['delivered', 'returned'],
-      delivered: [],
-      cancelled: [],
-      returned: [],
-    };
-    if (!allowedTransitions[order.status]?.includes(toStatus)) {
-      throw new Error(`Order status has already changed by another admin or Invalid order status transition: ${order.status} → ${toStatus}`);
-    }
-
     const cartItems = order.cartItems;
     if (cartItems.length === 0) {
       throw new Error('Order has no cart items');
@@ -60,10 +45,12 @@ const updateOrderStatusIntoDB = async (id: string, fromStatus: TStatus, toStatus
     //====================== CONFIRM ORDER ======================
     if (toStatus === 'confirmed') {
       //check quantity <= stock_quantity----------------------------
-      //The bulkWrite-filter will check this condition in the next step, so this loop is technically redundant here.
-      //Trade-off:
-      //->Loop : slightly less performance, better error message (product name)
-      //->No loop : fewer DB calls, maximum performance
+      /*
+      The bulkWrite-filter will check this condition in the next step, so this loop is technically redundant here.
+      Trade-off:
+      ->Loop : slightly less performance, better error message
+      ->bulkWrite : fewer DB calls, maximum performance
+      */
       for (const item of cartItems) {
         const product = await Product.findById(item.product_id).session(session);
         if (!product) {
@@ -73,46 +60,8 @@ const updateOrderStatusIntoDB = async (id: string, fromStatus: TStatus, toStatus
           throw new Error(`Insufficient stock for ${product.name}`);
         }
       }
-      /*
-      //Better performance than above loop-> Instead of looping findById for each product fetch all products at once. Reduces N DB calls → 1 DB call. Only useful if cart can have many items.
-      const productIds = cartItems.map((i) => i.product_id);
-      const products = await Product.find({ _id: { $in: productIds } }).session(session);
-      cartItems.forEach((item) => {
-        const product = products.find((p) => p._id.equals(item.product_id));
-        if (!product) throw new Error('Product not found');
-        if (item.quantity > product.stock_quantity)
-          throw new Error(`Insufficient stock for ${product.name}`);
-      });
-      */
 
       //all checks passed → now deduct stock----------------------------
-      /*
-      //not fully optimized
-      for (const item of cartItems) {
-        await Product.findByIdAndUpdate(
-          item.product_id,
-          {
-            $inc: { stock_quantity: -item.quantity },
-          },
-          { session },
-        );
-      }
-      */
-      /*
-      optimize: bulkWrite-> Instead of 10 update calls → 1 call with 10 updates at a time
-      But here is a problem also. Right now:
-      ->You read stock_quantity
-      ->Then later update(deduct) stock_quantity
-      ->Between those two steps, another transaction could reduce stock.
-      const bulkOps = cartItems.map((item) => ({
-        updateOne: {
-          filter: { _id: item.product_id },
-          update: { $inc: { stock_quantity: -item.quantity } },
-        },
-      }));
-      await Product.bulkWrite(bulkOps, { session });
-      */
-      //final version -> add checking stock_quantity inside bulkWrite filter, this makes your system race-condition safe.
       const bulkOps = cartItems.map((item) => ({
         updateOne: {
           filter: {
@@ -123,67 +72,35 @@ const updateOrderStatusIntoDB = async (id: string, fromStatus: TStatus, toStatus
         },
       }));
       const bulkResult = await Product.bulkWrite(bulkOps, { session });
-      //if (bulkResult.modifiedCount !== cartItems.length) {
-      //better than modifiedCount
       if (bulkResult.matchedCount !== cartItems.length) {
         throw new Error('One or more products are out of stock! Please retry.');
       }
-
-      //and now update the order status---------------------------------------
-      const updatedOrder = await Order.findOneAndUpdate(
-        { _id: id, status: fromStatus, __v: order.__v },
-        { status: 'confirmed', $inc: { __v: 1 } },
-        { new: true, session },
-      );
-      if (!updatedOrder) {
-        throw new Error('Order was already confirmed or cancelled by another admin.');
-      }
-
-      await session.commitTransaction();
-      return updatedOrder;
     }
 
     //====================== CANCEL / RETURN ORDER ======================
     if (toStatus === 'cancelled' || toStatus === 'returned') {
-      //restock only if stock was already deducted
-      //confirmed, processing, shipped → restock
-      const restockAbleStatuses: TStatus[] = ['confirmed', 'processing', 'shipped'];
-
+      const restockAbleStatuses: TStatus[] = ['confirmed', 'processing', 'shipped']; //restock only if stock was already deducted
       if (restockAbleStatuses.includes(order.status)) {
-        //bulk restock---------------------------------------
         const bulkOps = cartItems.map((item) => ({
           updateOne: {
             filter: { _id: item.product_id },
             update: { $inc: { stock_quantity: item.quantity } },
           },
         }));
-
         await Product.bulkWrite(bulkOps, { session });
       }
-
-      //update order status---------------------------------------
-      const updatedOrder = await Order.findOneAndUpdate(
-        { _id: id, status: fromStatus, __v: order.__v },
-        { status: toStatus, $inc: { __v: 1 } },
-        { new: true, session },
-      );
-      if (!updatedOrder) {
-        throw new Error('Order was already updated by another admin.');
-      }
-
-      await session.commitTransaction();
-      return updatedOrder;
     }
 
-    //====================== OTHER STATUS UPDATES ======================
-    //non-confirmed status update
+    //====================== UPDATE ORDER STATUS ======================
     const updatedOrder = await Order.findOneAndUpdate(
       { _id: id, status: fromStatus, __v: order.__v },
       { status: toStatus, $inc: { __v: 1 } },
       { new: true, session },
     );
     if (!updatedOrder) {
-      throw new Error('Order was already updated by another admin.');
+      throw new Error(
+        `Unable to update order. Current status: ${order.status}. It may have already been modified by another admin.`,
+      );
     }
 
     await session.commitTransaction();
