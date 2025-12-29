94b6d1ef46fd8812aaeff5ec93ee84732277dabf
bayazidhassan <bayazidhassan776@gmail.com>
2025-12-29 06:19:28 +0600

    credentials: true

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,8 @@ import router from './app/routes';
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({ origin: ['https://fitnestbd.netlify.app', 'http://localhost:5173'], credentials: true }),
-);
+app.use(cors({ origin: ['https://fitnestbd.netlify.app', 'http://localhost:5173'] }));
+//credentials: true -> allows browser to send cookies
 
 app.use('/api/v1', router);
 
